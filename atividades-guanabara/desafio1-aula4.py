a9ed69d2ad15768a098505b9693b8821706b512a
Caua5xx <cauaamaral32@gmail.com>
2026-01-06 20:22:09 -0300

    desafio 01 aula 04 do guanabara

---
+++ atividades-guanabara/desafio1-aula4.py
@@ -0,0 +1,7 @@
+#Desafio 01 da aula 04.
+
+#Crie um script Python que leia o nome de uma pessoa e mostre 
+#uma mensagem de boas-vindas de acordo com o valor digitado. 
+
+nome = input("Qual é seu nome?")
+print(f"Olá {nome}! seja Bem-Vindo!")
