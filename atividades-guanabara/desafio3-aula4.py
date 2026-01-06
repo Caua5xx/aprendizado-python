74f5186ff3fa4c25774b20f19f06078bad029345
Caua5xx <cauaamaral32@gmail.com>
2026-01-06 20:49:17 -0300

    desafio 03 aula 04 do guanabara

---
+++ atividades-guanabara/desafio3-aula4.py
@@ -0,0 +1,8 @@
+#Desafio 03 da aula 04 do guanabara
+
+#Crie um script Python que leia dois números e tente mostrar
+#a soma entre eles.
+
+num1 = float(input("Primeiro número: "))
+num2 = float(input("Segundo número: "))
+print(num1 + num2)
